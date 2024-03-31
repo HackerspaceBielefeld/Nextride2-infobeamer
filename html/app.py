7f674a27f6804c6d76dac43d28ce44bb9b753a5b
inflac <inflac@inflacsan.de>
2024-03-31 19:48:43 +0200

    add 404 error page

--- html/app.py
+++ html/app.py
@@ -107,5 +107,9 @@ def upload_result():
 def faq():
     return render_template('faq.html')
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
