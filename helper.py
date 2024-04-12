1c54e09a89f6e84413cd47db7758d5ace5ec5f2e
inflac <inflac@inflacsan.de>
2024-04-12 08:59:05 +0200

    fix pylint issus

--- helper.py
+++ helper.py
@@ -3,13 +3,25 @@ import xml.etree.ElementTree as ET
 import requests
 
 
-def fetch_schedule(schedule_uri:str):
+import requests
+
+def fetch_schedule(schedule_uri: str, timeout: int = 10) -> str:
+    """
+    Fetches schedule data from a given URI.
+
+    Args:
+        schedule_uri (str): The URI to fetch the schedule data from.
+        timeout (int, optional): The timeout value for the HTTP request in seconds. Defaults to 10.
+
+    Returns:
+        str: The fetched schedule data if successful, None otherwise.
+    """
     try:
-        response = requests.get(schedule_uri)
+        response = requests.get(schedule_uri, timeout=timeout)
         if response.status_code == 200:
             return response.text
         print("Failed to fetch XML. Status code:", response.status_code)
-    except Exception as e:
+    except requests.exceptions.RequestException as e:
         print("An error occurred while fetching XML:", str(e))
     return None
 
