66db6b0ba30180b105844e994eded4d4a3496972
inflac <inflac@inflacsan.de>
2024-04-12 14:40:05 +0200

    fix pylint issues in queuehandler

--- html/queuehandler.py
+++ html/queuehandler.py
@@ -1,22 +1,36 @@
 import os
 
-from db_file_helper import get_file_from_queue, get_file_from_uploads
-from db_file_helper import add_file_to_uploads
-from db_file_helper import remove_file_from_queue, remove_file_from_uploads
+from db_file_helper import (get_file_from_queue,add_file_to_uploads,
+    remove_file_from_queue, remove_file_from_uploads)
 from filehandler import check_file_exist, move_file
 from emailhandler import sent_email_error_message
 from helper import logging, hash_sha_512
 
 def approve_file(file_name, uploads_path:str, file_password:str, admin=False):
+    """
+    Approves a file from the queue.
+
+    Args:
+        file_name (str): The name of the file to approve.
+        uploads_path (str): The path where approved files are stored.
+        file_password (str): The password associated with the file.
+        admin (bool, optional): Whether the approval is done by an admin. Defaults to False.
+
+    Returns:
+        bool: True if the file was successfully approved, False otherwise.
+    """
+
     file_to_approve = get_file_from_queue(file_name)
     if not file_to_approve:
         logging("No db entry for requested file, nothing approved")
         return False
-    
+
     if not check_file_exist(file_to_approve.file_path):
-        logging(f"The requested file do not exist here: {file_to_approve.file_path}\n But a db entry for the file exist.")
-        error_message = '''While trying to approve an image a database inconsistance was detected.
-        The image which was requested to be approved, has a database entry but do not exist in the queue folder.'''
+        logging(f"The requested file does not exist: {file_to_approve.file_path}, "
+                "but a database entry for the file exists.")
+        error_message = ("While trying to approve a file, a database inconsistency was detected. "
+                         "The file requested to be approved has a database entry but does not "
+                         "exist in the queue folder.")
         sent_email_error_message("Database inconsistence", error_message)
 
     if file_to_approve.file_password != hash_sha_512(file_password) and not admin:
@@ -28,21 +42,23 @@ def approve_file(file_name, uploads_path:str, file_password:str, admin=False):
         return False
     
     if not remove_file_from_queue(file_to_approve.file_name):
-        logging("File couldn't be removed from queue table - Now trying to remove it from uploads again")
+        logging("File couldn't be removed from the queue table -"
+            "Now trying to remove it from uploads again")
         if not remove_file_from_uploads(file_to_approve.file_name, file_to_approve.id):
             logging("The file couldn't be removed from the uploads table")
-            error_message = '''While trying to approve a file, it was added to the uploads table but
-            while removing it from the queue table an error occured. Trying to also remove it from the
-            uploads table again failed.'''
-        sent_email_error_message("Database inconsistence", error_message)       
+            error_message = ("While trying to approve a file, it was added to the uploads table, "
+                "but while removing it from the queue table, an error occurred. "
+                "Trying to also remove it from the uploads table again failed.")
+            sent_email_error_message("Database inconsistency", error_message)
         return False
 
     if not move_file(file_to_approve.file_path, destination_path):
-        logging("Moving the file from queue to uploads went wrong - Database changes already done.")
-        error_message = '''Moving an approved file went wrong. Because the database entries were already made
-        The image now needs to be moved manually. Also supervision is necessary to make sure
-        this don't happen again.'''
-        sent_email_error_message("Database inconsistence", error_message)       
+        logging("Moving the file from the queue to uploads went wrong - Database changes already done.")
+        error_message = ("Moving an approved file failed."
+            "Because the database entries were already made, "
+            "the image now needs to be moved manually. Supervision is necessary to ensure "
+            "this does not happen again.")
+        sent_email_error_message("Database inconsistency", error_message)
         return False
     return True
 
