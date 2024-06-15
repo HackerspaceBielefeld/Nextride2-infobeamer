1132f92fa699858d6b7b907d40d2b2fc993f2432
inflac <inflac@inflacsan.de>
2024-06-15 22:39:12 +0200

    fix issues deleting images from dashboard

--- html/helper.py
+++ html/helper.py
@@ -83,7 +83,7 @@ def sanitize_string(content:str, extend_allowed_chars=False):
         str: sanitized string.
     """
     pattern = r'a-zA-Z0-9_\-.' # RE pattern with whitelisted chars
-    if extend_allowd_chars: pattern = r"a-zA-Z0-9_\-.\s"
+    if extend_allowed_chars: pattern = r"a-zA-Z0-9_\-.\s"
     # Replace chars that aren't whitelisted
     sanitized_content = re.sub(f'[^{pattern}]', "", content)
     return sanitized_content
